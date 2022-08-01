306c460d8d52e5ab288a0821c4631ae29d8d6746
Artur Kraft <hi@artur.kr>
2022-08-01 20:04:33 +0100

    sync

---
+++ config.php
@@ -0,0 +1,40 @@
+<?php
+
+$generate_dates = 0;
+$generate_ics_dates = 0;
+$show_octopus = 0;
+$schedule_code='2132';
+$weeks_added='3442';
+$ics_end_date = '2022-12-15';
+
+$location_name = "Brookfield";
+$folder_name = "brookfield";
+
+$greyStartDate = '2022-01-02';
+$greyEndDate = '2025-12-31';
+$greyColour = '#737373';
+
+$blueStartDate = '2022-01-02';
+$blueEndDate = '2025-12-31';
+$blueColour = '#007DBB';
+
+$greenStartDate = '2022-01-16';
+$greenEndDate = '2025-12-31';
+$greenColour = '#038831';
+
+
+$brownStartDate = '2022-01-09';
+$brownEndDate = '2025-12-31';
+$brownColour = '#966757';
+
+
+$gtag = "<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src=\"https://www.googletagmanager.com/gtag/js?id=G-882W2GEB0V\"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-882W2GEB0V');
+</script>";
+
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+define('__ROOT__', dirname(dirname(__FILE__)));
+
+
+
+require_once(__ROOT__.'/brookfield/config.php');
+
+$bins_main = 1;
+
+require_once(__ROOT__.'/bins-main/index.php');
